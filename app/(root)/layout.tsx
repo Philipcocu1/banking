cecc1a2a62bd9e8734d74702bf137964f4250c87
philipcocu1 <chrisphilonline@gmail.com>
2024-09-04 15:43:39 +0100

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,10 @@
+// import { Sidebar } from "lucide-react";
+
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect } from "next/navigation";
  
 
 
@@ -6,10 +13,38 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+const loggedIn={firstName:'Smith' , lastName:'Ronaldo'};
+
+  // return (
+  //  <main className="flex h-screen w-full font-inter">
+  //   <Sidebar user={loggedIn}/>
+  //  <div className="flex size-full flex-col">
+  //   <div className="root-layout">
+  // <Image src="/icons/logo.svg/" width={30} height={30} alt="menu icon"/>
+  // <div>
+  //   <MobileNav/>
+  // </div>
+  //   </div>
+  //   {children}
+  //  </div>
+
+    
+  //  </main>
+  // );
   return (
-   <main>
-    SIDEBAR
-    {children}
-   </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,53 @@
+
+import Headerbox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar';
 import React from 'react'
 
+
+
+
+
+
+
+
+
+
+
+
 const Home = () => {
+const logginIn={firstName: "Smith", lastName:"Ronaldo",email:"ronaldo@gmail.com"};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+   <div className='home-content'>
+  <header className='home-header'>
+      <Headerbox 
+      type='greeting'
+      title="Welcome"
+      user={logginIn?.firstName || "Guest"}
+      subtext="Access and Manage your account and transaction efficiently"
+      />
+     <TotalBalanceBox
+     accounts={[]}
+     totalBanks={1}
+     totalCurrentBalance={25000.00}
+/>
+  </header>
+
+  
+Recent Transactions
+
+   </div>
+  <RightSidebar
+  
+  user={logginIn}
+  transactions={[]}
+  banks={[{},{}]}
+  
+  />
+       
+    </section>
   )
 }
 
